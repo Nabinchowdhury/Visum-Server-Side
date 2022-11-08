7718d83f540a9b5608967d7c230e2a2c88ae6b2f
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-11-08 19:58:46 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`)
+})
+app.get("/", (req, res) => {
+    res.send("Service Review Server is Running")
+})
