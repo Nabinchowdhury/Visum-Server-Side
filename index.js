3d12a8390035c5ab3cb38e2e4914425515cfe260
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-11-08 21:38:33 +0600

    add service byy post and sorted by date get services initialized

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -12,5 +13,37 @@ app.listen(port, () => {
     console.log(`Server is listening to port ${port}`)
 })
 app.get("/", (req, res) => {
+
     res.send("Service Review Server is Running")
 })
+
+const uri = process.env.DB_URL;
+// console.log(uri)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        const servicesCollection = client.db("b6a11_serviceDB").collection("services")
+        const reviewsCollection = client.db("b6a11_serviceDB").collection("reviewa")
+
+
+        app.get("/services", async (req, res) => {
+            const query = {}
+            const cursor = servicesCollection.find(query).sort({ date: -1 })
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.post("/addService", async (req, res) => {
+            const service = req.body
+            console.log(service)
+            const result = await servicesCollection.insertOne(service)
+            res.send(result)
+        })
+
+    } finally {
+
+    }
+}
+
+run().catch(err => console.log(err))
